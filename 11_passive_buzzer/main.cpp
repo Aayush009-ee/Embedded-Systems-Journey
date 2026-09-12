021794479bbdd73e3c8e2928eada5c0bf9e6bb58
Aayush009-ee <aayushnair009@gmail.com>
2026-09-12 12:17:28 +0530

    Update main.cpp

--- 11_passive_buzzer/main.cpp
+++ 11_passive_buzzer/main.cpp
@@ -5,7 +5,7 @@ const int buzzer = 8;
 void setup() {
 
     tone(buzzer, 10000);   // Generate 1000 Hz tone
-    delay(1000);           // Play for 0.5 seconds
+    delay(5000);           // Play for 0.5 seconds
 
     noTone(buzzer);       // Stop the tone
 }
