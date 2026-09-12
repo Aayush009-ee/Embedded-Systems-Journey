b5ad1b915cfc2f9dfb47ffc1508f45ea96a74764
Aayush009-ee <aayushnair009@gmail.com>
2026-09-12 12:02:45 +0530

    Create main.cpp

---
+++ 11_passive_buzzer/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+
+const int buzzer = 8;
+
+void setup() {
+
+    tone(buzzer, 10000);   // Generate 1000 Hz tone
+    delay(1000);           // Play for 0.5 seconds
+
+    noTone(buzzer);       // Stop the tone
+}
+
+void loop() {
+    
+}
